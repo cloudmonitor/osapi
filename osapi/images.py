b4a5b53ff5f6d3483164bdbeb821767cb5ef439b
henulwj <henulwj@qq.com>
2017-01-17 10:17:16 +0800

    修改获取镜像名字bug

--- osapi/images.py
+++ osapi/images.py
@@ -14,6 +14,7 @@ def get_tenant_images(token_id):
 def get_image_name(token_id, image_id):
     """通过image_id获取image的名字"""
     images_info = get_tenant_images(token_id)
+    image_name = "unknown"
     for i in range(len(images_info["images"])):
         if image_id == images_info["images"][i]["id"]:
             image_name = images_info["images"][i]["name"]
@@ -27,3 +28,4 @@ if __name__ == "__main__":
     admin_token_id = admin_token['access']['token']['id']
     admin_tenant_id = admin_token['access']['token']['tenant']['id']
     print json.dumps(get_tenant_images(admin_token_id))
+    print get_image_name(admin_token_id, "add08de8-d663-4022-86a9-1b1c39a8653c")
